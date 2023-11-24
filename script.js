df87a5db611859fd1047342e3567801a7eda3361
hardikk04 <hardik04062003@gmail.com>
2023-11-24 12:57:17 +0530

    page5 done

--- script.js
+++ script.js
@@ -43,3 +43,54 @@ t1.to(".page2-img", {
     // markers: true,
   },
 });
+
+t1.to(".page4-elem", {
+  height: "26.6vh",
+  stagger: 0.5,
+  scrollTrigger: {
+    scroller: "body",
+    trigger: ".page4",
+    start: "top 0%",
+    pin: true,
+    scrub: 1,
+    // markers: true,
+  },
+});
+
+t1.to(".plus2", {
+  transform: "rotate(0deg)",
+  stagger: 0.5,
+  scrollTrigger: {
+    scroller: "body",
+    trigger: ".page4",
+    start: "top 0%",
+    scrub: 1,
+    // markers: true,
+  },
+});
+
+t1.to(".page4-elem-heading > h1", {
+  color: "#FF2A1D",
+  stagger: 0.5,
+  scrollTrigger: {
+    scroller: "body",
+    trigger: ".page4",
+    start: "top 0%",
+    scrub: 1,
+    // markers: true,
+  },
+});
+
+t1.to(".page5-img", {
+  transform: "translateX(-200%)",
+
+  scrollTrigger: {
+    scroller: "body",
+    trigger: ".page5",
+    start: "top 0%",
+    end: "top -200%",
+    pin: true,
+    scrub: 1,
+    // markers: true,
+  },
+});
